a0ec1aa6673100649d99c1d304626e17daae3eeb
shin seung jae <shinseungjae1234@gmail.com>
2022-03-25 14:16:18 +0900

    Apploading for preload assets and fonts

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { Asset, useAssets } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Text } from "react-native";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) => {
+  if (typeof image == "string") {
+    return Image.prefetch(image);
+  } else {
+    return Asset.loadAsync(image);
+  }
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    // preloading
+    const fonts = loadFonts([Ionicons.font]);
+    //const images = loadImages([require("./path"), "url"]);
+    await Promise.all([...fonts]);
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+  return <Text>I finish my loading!!</Text>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
